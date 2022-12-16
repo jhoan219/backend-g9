5f76381475f2636ca088adf736cf95838e00f7fc
jhoan219 <labrajhoan219@gmail.com>
2022-12-16 16:13:21 -0500

    finalizacion de semna 5

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from flask_restful import Api
 from config import conexion
 from models.usuarios import UsuarioModel
 from models.tareas import TareaModel
-from controllers.usuarioController import UsuariosController
+from controllers.usuarioController import UsuariosController, UsuarioController
 from controllers.pruebaController import PruebaController
+from controllers.tareaController import TareasController, TareaController
+
 # Para cargar las variables del archivo .env para que puedan ser utilizadas como variables de entorno
 load_dotenv()
 
@@ -31,7 +33,10 @@ migrate = Migrate(app, conexion)
 
 # Declarar todas las rutas que vamos a utilizar mediante los controladores
 api.add_resource(UsuariosController, '/usuarios')
- 
 api.add_resource(PruebaController, '/prueba')
+api.add_resource(UsuarioController, '/usuario/<int:id>')
+api.add_resource(TareasController, '/tareas')
+api.add_resource(TareaController, '/tarea/<int:usuarioId>')
+
 if __name__ == '__main__':
     app.run(debug=True)
